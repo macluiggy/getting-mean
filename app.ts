84caac215b2f54fd789e6ef412f87da96b33aabe
luiggy macias <ferrinluiggy@gmail.com>
2022-04-11 11:40:49 -0500

    trff

--- app.ts
+++ app.ts
@@ -40,5 +40,12 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+if (process.env.NODE_ENV !== "dev") {
+  let port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
 export default app;
 export const viteNodeApp = app;
